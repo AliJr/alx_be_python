1e08764612f835475bbf8a838c4c1a62aa16c45b
Ali Abdelhakam <aliabdo96@gmail.com>
2024-09-28 18:40:43 +0300

    variable names change question 3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,21 +1,26 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-
+FAHRENHEIT_OFFSET = 32
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 
 def convert_to_fahrenheit(celsius):   
-    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + FAHRENHEIT_OFFSET
 
 
-
-current_temp=input("Enter the temperature to convert: ")
-temp_type=input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-if temp_type=="C":
-    print(current_temp+"°C is",str(convert_to_fahrenheit(float(current_temp)))+"°F")
-elif temp_type=="F":
-    print(current_temp+"°F is",str(convert_to_celsius(float(current_temp)))+"°C")
-else:
-    print("Invalid input")
+def main():
+    current_temp=input("Enter the temperature to convert: ")
+    temp_type=input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+    if temp_type=="C":
+        print(current_temp+"°C is",str(convert_to_fahrenheit(float(current_temp)))+"°F")
+    elif temp_type=="F":
+        print(current_temp+"°F is",str(convert_to_celsius(float(current_temp)))+"°C")
+    else:
+        print("Invalid input")
+    
+    
+# Run the program
+if __name__ == "__main__":
+    main()
